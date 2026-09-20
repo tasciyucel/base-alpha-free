944afe83d1aaf9710e1845ded81d341ba04cdb53
tasciyucel <85459034+tasciyucel@users.noreply.github.com>
2026-09-20 17:44:11 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+base-alpha-free/
+│
+├── scanner.py
+├── database.py
+├── config.py          ← şu anda bunu oluştur
+├── requirements.txt
+├── .gitignore
+│
+└── .github/
+    └── workflows/
+        └── scanner.yml
